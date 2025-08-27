f60a839735a45bdad7da4a0f26187f2971ba96a3
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-08-27 19:31:42 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -339,12 +339,7 @@ class LongitudinalMpc:
     # do not apply to deceleration
     j_ego_v_ego = 1
     a_change_v_ego = 1
-    if v_ego <= low_thr:
-      j_comf = 5
-    elif v_ego >= high_thr:
-      j_comf = 20
-    else:
-      j_comf = 1
+    j_comf = 5 if v_ego <= low_thr else (20 if v_ego >= high_thr else 1)
     if (v_lead0 - v_ego >= 0) and (v_lead1 - v_ego >= 0):
       j_ego_v_ego = np.interp(v_ego, v_ego_bps, [.10, 1.])
       a_change_v_ego = np.interp(v_ego, v_ego_bps, [.10, 1.])
