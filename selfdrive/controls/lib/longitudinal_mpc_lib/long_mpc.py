f038e2751b191e332071751e8952443f8ac3f147
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-09-12 17:10:34 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -517,7 +517,7 @@ class LongitudinalMpc:
       #w_raw = (v_ego - low_thr) / max(1e-6, (high_thr - low_thr))
       w_raw = v_ego / max(1e-6, (high_thr - low_thr))
       w = np.clip(w_raw, 0.0, 0.98)
-      
+      #w = np.clip(w_raw + 0.2, 0.0, 1.0)
       x_mixed = (1 - w) * np.min(x_and_cruise, axis=1) + w * np.max(x_and_cruise, axis=1)
       x = x_mixed
       #================================================================
