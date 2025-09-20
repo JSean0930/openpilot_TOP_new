1060a19ee68e259ff4b492a8dbbf3042cabe4846
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-09-20 15:25:52 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -72,7 +72,7 @@ def get_danger_zone_cost(v_ego):
   if v_ego <= low_thr:
     return 300.0
   elif v_ego <= mid_thr:
-    return 150.0
+    return 200.0
   else:
     return 200.0
 #===================================================================
