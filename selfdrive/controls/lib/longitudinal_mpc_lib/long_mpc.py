34c6c6ef877494fc165621225f14f281e0d1316e
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-09-22 20:17:04 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -70,7 +70,7 @@ high_thr = 70.0 / 3.6
 #===================================================================
 def get_danger_zone_cost(v_ego):
   if v_ego <= low_thr:
-    return 250.0
+    return 300.0
   elif v_ego <= mid_thr:
     return 200.0
   else:
