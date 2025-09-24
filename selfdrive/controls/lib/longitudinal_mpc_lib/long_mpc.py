64aec561749849d58bbaa16ad64a8c77e3aac132
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-09-24 17:58:48 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -161,9 +161,9 @@ def get_stopped_equivalence_factor(v_lead, v_ego):
   v_diff_offset_max = 10
   speed_to_reach_max_v_diff_offset = 15 # in kp/h
   speed_to_reach_max_v_diff_offset = speed_to_reach_max_v_diff_offset * CV.KPH_TO_MS
-  delta_speed = v_lead - v_ego
+  delta_speed = v_lead * 2 - v_ego
   if np.all(delta_speed > 0.5):
-    v_diff_offset = delta_speed * 8
+    v_diff_offset = delta_speed * 2
     v_diff_offset = np.clip(v_diff_offset, 0, v_diff_offset_max)
     v_diff_offset = np.maximum(v_diff_offset * ((speed_to_reach_max_v_diff_offset - v_ego)/speed_to_reach_max_v_diff_offset), 0)
   return (v_lead**2) / (2 * COMFORT_BRAKE) + v_diff_offset
