22befde1b116413856348a3d2e2645c5e6772796
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-10-07 17:22:07 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -52,7 +52,7 @@ ACADOS_SOLVER_TYPE = 'SQP_RTI'
 # much better convergence of the MPC with low iterations
 N = 12
 MAX_T = 10.0
-T_IDXS = (np.linspace(0, 1, N + 1) ** 2.0) * MAX_T
+T_IDXS = (np.linspace(0, 1, N + 1) ** 1.65) * MAX_T
 #T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1)]
 
 #T_IDXS = np.array(T_IDXS_LST)
