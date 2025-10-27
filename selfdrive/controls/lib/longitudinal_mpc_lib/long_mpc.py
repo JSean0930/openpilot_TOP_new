f31b56dc696df21bcb1db6df4a2c5f79e2ecca9b
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-10-28 07:50:17 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -59,7 +59,7 @@ T_IDXS = (np.linspace(0, 1, N + 1) ** 1.65) * MAX_T
 #T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-COMFORT_BRAKE = 1.8
+COMFORT_BRAKE = 1.85
 # STOP_DISTANCE = 6.0
 CRUISE_MIN_ACCEL = -1.2
 CRUISE_MAX_ACCEL = 1.6
